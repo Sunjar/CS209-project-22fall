fd4275b2830b1505c229c8fe5ca1cfaad47e8add
hzr764108561 <764108561@qq.com>
2022-12-19 15:46:35 +0800

    ...

---
+++ src/test/java/com/example/springproject/SpringprojectApplicationTests.java
@@ -0,0 +1,30 @@
+package com.example.springproject;
+
+import com.example.springproject.controller.getInfo;
+import com.example.springproject.domain.commitHour;
+import com.example.springproject.domain.issueAnalyse;
+import com.example.springproject.service.AddInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class SpringprojectApplicationTests {
+    @Autowired
+    AddInfo addInfo;
+	@Test
+    void testInsertRepos(){
+//        InfoController infoController1 = new InfoController();
+//        String url = "https://api.github.com/repos/z-pattern-matching/z";
+//        infoController1.addInfos(url);
+        getInfo getInfo = new getInfo();
+        List<issueAnalyse> z = getInfo.gerIssuesAnalysis(1);
+        List<commitHour> x = getInfo.getCommitByHourRelease(1);
+//
+       int a = 0;
+    }
+
+}
+
